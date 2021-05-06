ed29c924acdc7af60bdd6bba84834afe300281dd
Julia Shevtsova <juliashevtsova96@gmail.com>
2021-05-06 16:22:07 +0300

    Добавьте файлы проекта.

---
+++ Tic-tac-toe/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tic_tac_toe
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
